b3dd11cc5bce752b55bb10d7237127e3751fbb02
Gaston Florenciano <gaston__florenciano@hotmail.com>
2026-02-28 21:54:33 -0300

    style: :lipstick: Estilos en la navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,24 @@
 "use client"
+import Image from "next/image"
 
 function NavBar() {
   return (
-    <nav className="bg-gradient-to-r from-orange-300 via-orange-500 to-orange-300 text-white py-2 fixed w-full top-0 left-0 z-50 border-b border-orange-400 shadow-md">
-      <h1 className="text-4xl md:text-5xl font-navbar text-center drop-shadow-sm">
+    <nav className="bg-white text-white py-2 fixed w-full top-0 left-0 z-50 border-b border-orange-400 shadow-md flex justify-center items-center gap-4 h-[80px]">
+      <Image
+        src="/palms.gif"  // 👈 Next.js ya sabe que debe buscar en la carpeta public
+        alt="Palms"
+        width={40}
+        height={40}
+      />
+      <h1 className="text-4xl md:text-5xl font-bold font-navbar drop-shadow-sm text-orange-400">
         Quincho Doña Leonarda
-      </h1>    
+      </h1>
+      <Image
+        src="/palms.gif"  // 👈 Next.js ya sabe que debe buscar en la carpeta public
+        alt="Palms"
+        width={40}
+        height={40}
+      />
     </nav>
   )
 }
